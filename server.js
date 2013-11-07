ceb4734a912c5f4621fce9681336b49008a75b61
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-07 14:58:00 +0100

    skeletal server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = new express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+app.set('port', process.env.port || 1729);
+
+io.sockets.on('connection', function(socket){
+    console.log('socket %s connected', socket.id);
+});
+
+server.listen(app.get('port'));
+console.log('server started at port %s', app.get('port'));
